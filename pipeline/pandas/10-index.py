2edb6368933b8c08a3c225bd5b0f305a17e507cb
ShamilAliyew <shamil.aliye005@gmail.com>
2026-01-08 21:28:49 +0400

    Done

--- pipeline/pandas/10-index.py
+++ pipeline/pandas/10-index.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """a function def index(df): that takes a pd.DataFrame and:
 Sets the Timestamp column as the index of the dataframe"""
+import pandas as pd
 
 
 def index(df):
     """a function def index(df): that takes a pd.DataFrame and:
     Sets the Timestamp column as the index of the dataframe"""
-
+    df["Timestamp"] = pd.to_datetime(df["Timestamp"], unit='s')
     df = df.set_index("Timestamp", drop=True)
     return df

f4bfb67ea2a88b5704c2292ef0040e465408dbe1
ShamilAliyew <shamil.aliye005@gmail.com>
2026-01-08 17:24:57 +0400

    Done

--- pipeline/pandas/2-from_file.py
+++ pipeline/pandas/2-from_file.py
@@ -2,6 +2,8 @@
 import pandas as pd
 """ a function def from_file(filename, delimiter):
  that loads data from a file as a pd.DataFrame"""
+
+
 def from_file(filename, delimiter):
     """ a function def from_file(filename, delimiter):
      that loads data from a file as a pd.DataFrame"""

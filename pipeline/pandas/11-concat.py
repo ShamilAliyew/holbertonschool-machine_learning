a340f3b95861a19c088b16ca8e72471c86125322
ShamilAliyew <shamil.aliye005@gmail.com>
2026-01-08 21:33:50 +0400

    Done

--- pipeline/pandas/11-concat.py
+++ pipeline/pandas/11-concat.py
@@ -8,6 +8,6 @@ def concat(df1, df2):
     """a function def concat(df1, df2): that takes two pd.DataFrame objects"""
     df1 = index(df1)
     df2 = index(df2)
-    updated_df2 = df2.loc[:pd.to_datetime(1417411920, units='s')]
+    updated_df2 = df2.loc[:1417411920]
     df = pd.concat([updated_df2, df1], keys=["bitstamp", "coinbase"])
     return df

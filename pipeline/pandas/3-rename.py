a6983f9420946d1d5e6bc23d20368148408cb1cf
ShamilAliyew <shamil.aliye005@gmail.com>
2026-01-08 17:41:15 +0400

    Done

--- pipeline/pandas/3-rename.py
+++ pipeline/pandas/3-rename.py
@@ -8,5 +8,5 @@ def rename(df):
     """ a function def rename(df):
      that takes a pd.DataFrame as input and performs the following"""
     df.rename(columns={"Timestamp": "Datetime"},inplace=True)
-    df["Datetime"] = pd.to_datetime(df["Datetime"])
+    df["Datetime"] = pd.to_datetime(df["Datetime"], unit="s")
     return df

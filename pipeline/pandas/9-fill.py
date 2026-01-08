9d5f803952f01cedd7959ab991de28a3f2efc751
ShamilAliyew <shamil.aliye005@gmail.com>
2026-01-08 20:09:49 +0400

    Done

--- pipeline/pandas/9-fill.py
+++ pipeline/pandas/9-fill.py
@@ -10,8 +10,8 @@ Sets missing values in Volume_(BTC) and Volume_(Currency) to 0"""
 def fill(df):
     """a function def fill(df): that takes a pd.DataFrame"""
 
-    updated_df = df.drop(columns=["Weighted_Price"])
-    updated_df["Close"].fillna(method="ffill", inplace=True)
-    updated_df[["High","Low","Open"]] = updated_df[["High","Low","Open"]].fillna(updated_df["Close"], inplace=True, axis = 0)
-    updated_df["Volume_(BTC)","Volume_(Currency)"].fillna(0, inplace=True)
-    return updated_df
+    df = df.drop(columns=["Weighted_Price"])
+    df["Close"] = df["Close"].fillna(method="ffill", inplace=True)
+    df[["High", "Low", "Open"]] = df[["High","Low","Open"]].fillna(df["Close"], axis = 0)
+    df["Volume_(BTC)","Volume_(Currency)"] = df["Volume_(BTC)","Volume_(Currency)"].fillna(0, inplace=True)
+    return df

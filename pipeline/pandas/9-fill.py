8cbb1e530b95df98e0dd7e52a969d9bce575770e
ShamilAliyew <shamil.aliye005@gmail.com>
2026-01-08 19:56:56 +0400

    Done

---
+++ pipeline/pandas/9-fill.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""a function def fill(df): that takes a pd.DataFrame and:
+Removes the Weighted_Price column.
+Fills missing values in the Close column with the previous row’s value.
+Fills missing values in the High, Low, and Open columns with the
+corresponding Close value in the same row.
+Sets missing values in Volume_(BTC) and Volume_(Currency) to 0"""
+
+
+def fill(df):
+    """a function def fill(df): that takes a pd.DataFrame"""
+
+    updated_df = df.drop(columns=['Weighted_Price'])
+    updated_df['Close'].fillna(method='ffill', inplace=True)
+    updated_df['Close','Low','Open'].fillna(updated_df['Close'], inplace=True)
+    updated_df['Volume_(BTC)','Volume_(Currency)'].fillna(0,inplace=True)
+    return updated_df

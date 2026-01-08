d941eb20f5d7a79da0dbe4f85e6bd18058ee0864
ShamilAliyew <shamil.aliye005@gmail.com>
2026-01-08 20:37:13 +0400

    Done

--- pipeline/pandas/9-fill.py
+++ pipeline/pandas/9-fill.py
@@ -11,7 +11,7 @@ def fill(df):
     """a function def fill(df): that takes a pd.DataFrame"""
 
     df = df.drop(columns=["Weighted_Price"])
-    df["Close"] = df["Close"].fillna(method="ffill",inplace=True)
-    df[["High", "Low", "Open"]] = df[["High", "Low", "Open"]].fillna(df["Close"], axis=0 )
+    df["Close"] = df["Close"].fillna(method="ffill")
+    df[["High", "Low", "Open"]] = df[["High", "Low", "Open"]].fillna(df["Close"]).ffill()
     df[["Volume_(BTC)", "Volume_(Currency)"]] = df[["Volume_(BTC)", "Volume_(Currency)"]].fillna(value=0)
     return df

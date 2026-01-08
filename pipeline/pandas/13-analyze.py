c743ea7e57be5ad847aa6f72b2acaacc139a8fe5
ShamilAliyew <shamil.aliye005@gmail.com>
2026-01-08 22:06:13 +0400

    Done

---
+++ pipeline/pandas/13-analyze.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""a function def analyze(df): that takes a pd.DataFrame and:
+Computes descriptive statistics for all columns except
+ the Timestamp column"""
+
+
+def analyze(df):
+    """a function def analyze(df): that takes a pd.DataFrame and:
+    Computes descriptive statistics for all columns except
+     the Timestamp column"""
+    return df.describe(include="all")

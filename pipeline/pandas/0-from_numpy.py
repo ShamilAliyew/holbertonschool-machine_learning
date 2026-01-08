186addace8fddfc1c5b4606cfcfd456b5d45fd6d
ShamilAliyew <shamil.aliye005@gmail.com>
2026-01-08 16:53:33 +0400

    Done

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -3,6 +3,7 @@ import pandas as pd
 def from_numpy(array):
     n_columns = array.shape[1]
     if n_columns > 26:
-        raise ValueError("Number of columns cannot exceed 26 for A-Z column names")
+        raise ValueError("Number of columns"
+                         " cannot exceed 26 for A-Z column names")
     columns = [chr(65+i) for i in range(n_columns)]
-    return pd.DataFrame(array, columns = columns)
+    return pd.DataFrame(array, columns=columns)

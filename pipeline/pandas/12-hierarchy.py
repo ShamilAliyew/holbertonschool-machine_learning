558426ac0e5ee5f0cb034580f6364eca9deb9696
ShamilAliyew <shamil.aliye005@gmail.com>
2026-01-08 22:01:28 +0400

    Done

--- pipeline/pandas/12-hierarchy.py
+++ pipeline/pandas/12-hierarchy.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
-"""a function def concat(df1, df2): that takes two pd.DataFrame objects"""
+"""a function def concat(df1, df2):
+ that takes two pd.DataFrame objects"""
 import pandas as pd
 index = __import__('10-index').index
 
 
 def concat(df1, df2):
-    """a function def concat(df1, df2): that takes two pd.DataFrame objects"""
+    """a function def concat(df1, df2):
+     that takes two pd.DataFrame objects"""
     df1 = index(df1)
     df2 = index(df2)
     updated_df2 = df2.loc[:1417411920]
@@ -13,13 +15,12 @@ def concat(df1, df2):
     return df
 
 def hierarchy(df1, df2):
-    """a function def hierarchy(df1, df2): that takes two pd.DataFrame objects"""
+    """a function def hierarchy(df1, df2):
+     that takes two pd.DataFrame objects"""
     df1 = index(df1)
     df2 = index(df2)
     df = pd.concat([df2, df1], keys=["bitstamp", "coinbase"])
-    updated_df = df.swaplevel(0,1).sort_index()
-
+    updated_df = df.swaplevel(0, 1).sort_index()
     start = 1417411980
     end = 1417417980
-
     return updated_df.loc[start:end]

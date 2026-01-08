1376124b0b05945118ff826d6c1653d3ef660153
ShamilAliyew <shamil.aliye005@gmail.com>
2026-01-08 20:27:40 +0400

    Done

--- pipeline/pandas/9-fill.py
+++ pipeline/pandas/9-fill.py
@@ -12,6 +12,6 @@ def fill(df):
 
     df = df.drop(columns=["Weighted_Price"])
     df["Close"] = df["Close"].ffill()
-    df[["High", "Low", "Open"]] = (df[["High","Low","Open"]].fillna(df["Close"]).ffill())
-    df[["Volume_(BTC)","Volume_(Currency)"]] = (df[["Volume_(BTC)","Volume_(Currency)"]].fillna(value=0))
+    df[["High", "Low", "Open"]] = (df[["High", "Low", "Open"]].fillna(df["Close"]).ffill())
+    df[["Volume_(BTC)", "Volume_(Currency)"]] = (df[["Volume_(BTC)", "Volume_(Currency)"]].fillna(value=0))
     return df

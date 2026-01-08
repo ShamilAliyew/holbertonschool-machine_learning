1daaa1000442a845d6fc54076bbc1b7caba1cc5c
ShamilAliyew <shamil.aliye005@gmail.com>
2026-01-08 17:59:10 +0400

    Done

---
+++ pipeline/pandas/5-slice.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""a function def slice(df): that takes a pd.DataFrame and"""
+
+
+def slice(df):
+    """a function def slice(df): that takes a pd.DataFrame and"""
+    return df[["High", "Low", "Close", "Volume_BTC"]].head(60)

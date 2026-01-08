03f8f9bac3825cd637ee13eec96dc9bf6647bceb
ShamilAliyew <shamil.aliye005@gmail.com>
2026-01-08 19:30:23 +0400

    Done

--- pipeline/pandas/6-flip_switch.py
+++ pipeline/pandas/6-flip_switch.py
@@ -6,5 +6,5 @@
 def flip_switch(df):
     """a function def flip_switch(df):
      that takes a pd.DataFrame and sort and transpose"""
-    updated_df = df.sort_values(ascending=False)
+    updated_df = df.sort_values(by="Timestamp", ascending=False)
     return updated_df.T

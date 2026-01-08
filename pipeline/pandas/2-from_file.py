18242692384ca7649f6b443de7a3bc01da7da095
ShamilAliyew <shamil.aliye005@gmail.com>
2026-01-08 17:24:28 +0400

    Done

---
+++ pipeline/pandas/2-from_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import pandas as pd
+""" a function def from_file(filename, delimiter):
+ that loads data from a file as a pd.DataFrame"""
+def from_file(filename, delimiter):
+    """ a function def from_file(filename, delimiter):
+     that loads data from a file as a pd.DataFrame"""
+    df = pd.read_csv(filename, delimiter=delimiter)
+    return df

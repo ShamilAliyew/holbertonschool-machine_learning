9ce19a3c940da54430ac2c76a974915874236467
ShamilAliyew <shamil.aliye005@gmail.com>
2026-01-09 12:54:14 +0400

    Done

--- pipeline/pandas/14-visualize.py
+++ pipeline/pandas/14-visualize.py
@@ -9,7 +9,7 @@ df = df.drop(columns=["Weighted_Price"])
 df = df.rename(columns={"Timestamp": "Date"})
 df["Date"] = pd.to_datetime(df["Date"], unit="s")
 df = df.set_index("Date", drop=True)
-df["Close"] = pd.fillna(method="ffill", axis=0)
+df["Close"] = df["Close"].ffill()
 df[["High", "Low", "Open"]] = df[["High", "Low", "Open"]].fillna(df["Close"]).ffill()
 df[["Volume_(BTC)", "Volume_(Currency)"]] = df[["Volume_(BTC)", "Volume_(Currency)"]].fillna(0)
 print(df)
@@ -19,9 +19,13 @@ daily = df_2017.resample("D").agg({
     'Low': 'min',
     'Open': 'mean',
     'Close': 'mean',
-    'Volume(BTC)': 'sum',
-    'Volume(Currency)': 'sum'
+    'Volume_(BTC)': 'sum',
+    'Volume_(Currency)': 'sum'
 })
-plt.plot(df_2017["Date"], daily)
+plt.figure(figsize=[12,6])
+plt.plot(daily.index, daily["Close"], label="Close Price")
+#asaqi setirde her tarixe aid low ve high deyerlerin arasinda rengli sahe yaradiriq(tradingdeki kimi)
+plt.fill_between(daily.index, daily["Close"], daily["High"], alpha=0.2, label="Close Price")
+plt.title("Daily BTC Prices >2017")
 plt.legend()
 plt.show()

9eb4200b6d4267564ebcea7a7227f718b8be46d8
ShamilAliyew <shamil.aliye005@gmail.com>
2026-01-11 20:40:53 +0400

    Done

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -8,8 +8,6 @@ def bars():
     """to plot a stacked bar graph"""
     np.random.seed(5)
     fruit = np.random.randint(0, 20, (4,3))
-    plt.figure(figsize=(6.4, 4.8))
-
     people = ['Farrah', 'Fred', 'Felicia']
 
     apples = fruit[0]
@@ -17,14 +15,13 @@ def bars():
     oranges = fruit[2]
     peaches = fruit[3]
 
-    plt.bar(people, apples, label="Apples", color='red', width=0.5)
-    plt.bar(people, bananas, label="Bananas", color='yellow', width=0.5, bottom=apples)
-    plt.bar(people, oranges, label="Oranges", color='#ff8000', width=0.5, bottom=apples+bananas)
-    plt.bar(people, peaches, label="Peaches", color='#ffe5b4', width=0.5, bottom=apples+bananas+oranges
-            )
+    plt.bar(people, apples, width=0.5, color='red', label='Apples')
+    plt.bar(people, bananas, width=0.5, color='yellow', bottom=apples, label='Bananas')
+    plt.bar(people, oranges, width=0.5, color='#ff8000', bottom=apples+bananas, label='Oranges')
+    plt.bar(people, peaches, width=0.5, color='#ffe5b4', bottom=apples+bananas+oranges, label='Peaches')
 
     plt.ylabel("Quantity of Fruit")
-    plt.yticks(range(0, 81, 10))
+    plt.yticks(np.arange(0, 81, 10))
     plt.title("Number of Fruit per Person")
     plt.legend()
 

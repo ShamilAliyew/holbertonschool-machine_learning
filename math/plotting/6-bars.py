ad40f51587a823a00383aa04b0bbed7f4c316406
ShamilAliyew <shamil.aliye005@gmail.com>
2026-01-11 20:49:42 +0400

    Done

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 def bars():
     """to plot a stacked bar graph"""
     np.random.seed(5)
-    fruit = np.random.randint(0, 20, (4,3))
+    fruit = np.random.randint(0, 20, (4, 3))
     people = ['Farrah', 'Fred', 'Felicia']
 
     apples = fruit[0]
@@ -15,10 +15,14 @@ def bars():
     oranges = fruit[2]
     peaches = fruit[3]
 
-    plt.bar(people, apples, width=0.5, color='red', label='apples')
-    plt.bar(people, bananas, width=0.5, color='yellow', bottom=apples, label='bananas')
-    plt.bar(people, oranges, width=0.5, color='#ff8000', bottom=apples+bananas, label='oranges')
-    plt.bar(people, peaches, width=0.5, color='#ffe5b4', bottom=apples+bananas+oranges, label='peaches')
+    plt.bar(people, apples, width=0.5, color='red',
+            label='apples')
+    plt.bar(people, bananas, width=0.5, color='yellow',
+            bottom=apples, label='bananas')
+    plt.bar(people, oranges, width=0.5, color='#ff8000',
+            bottom=apples+bananas, label='oranges')
+    plt.bar(people, peaches, width=0.5, color='#ffe5b4',
+            bottom=apples+bananas+oranges, label='peaches')
 
     plt.ylabel("Quantity of Fruit")
     plt.yticks(np.arange(0, 81, 10))

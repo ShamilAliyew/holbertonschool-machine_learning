8542b7a1aa2f85f7a285c7528148ec37b0056a9b
ShamilAliyew <shamil.aliye005@gmail.com>
2026-01-14 20:24:22 +0400

    Done

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
+"""Return the integer value of the sum"""
 
 
 def summation_i_squared(n):
+    """Return the integer value of the sum"""
     if isinstance(n, int) and n > 0:
         return int((n*(n+1)*(2*n+1))/6)
     else:

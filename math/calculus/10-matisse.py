34e6765cb5c283d37ddad23f9b21347e9da8809d
ShamilAliyew <shamil.aliye005@gmail.com>
2026-01-14 21:03:58 +0400

    Done

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 """a function that calculates the derivative of a polynomial"""
 
+
 def poly_derivative(poly):
     """a function that calculates the derivative of a polynomial"""
-    if not isinstance(poly, list) or len(poly) == 0 or any(not isinstance(x,(int, float)) for x in poly):
+    if (not isinstance(poly, list) or len(poly)
+    == 0 or any(not isinstance(x, (int, float)) for x in poly)):
         return None
     if len(poly) == 1:
         return [0]
     derivative = []
-    for i in range (1, len(poly)):
+    for i in range(1, len(poly)):
         cof = poly[i] * i
         derivative.append(cof)
     return derivative

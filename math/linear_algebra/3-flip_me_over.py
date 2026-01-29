98c9db61f90870f5786796569c03ad6955226289
ShamilAliyew <shamil.aliyev005@gmail.com>
2026-01-30 02:21:18 +0400

    Done

---
+++ math/linear_algebra/3-flip_me_over.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""a function def matrix_transpose(matrix):
+that returns the transpose of a 2D matrix, matrix"""
+
+
+def matrix_transpose(matrix):
+    """a function def matrix_transpose(matrix):
+    that returns the transpose of a 2D matrix, matrix"""
+    transposed_matrix = []
+    for j in range(len(matrix[0])):
+        transposed_row = []
+        for i in range(len(matrix)):
+            transposed_row.append(matrix[i][j])
+        transposed_matrix.append(transposed_row)
+    return transposed_matrix

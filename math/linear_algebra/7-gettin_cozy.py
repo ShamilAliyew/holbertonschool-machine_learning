efffc9343a271962b03bca068c88f64accd0b414
ShamilAliyew <shamil.aliyev005@gmail.com>
2026-01-30 03:19:42 +0400

    Done

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -7,8 +7,6 @@ shape = __import__('2-size_me_please').matrix_shape
 def cat_matrices2D(mat1, mat2, axis=0):
     """a function def cat_matrices2D(mat1, mat2, axis=0):
      that concatenates two matrices along a specific axis"""
-    if shape(mat1) != shape(mat2):
-        return None
     if axis == 0:
         return mat1 + mat2
     elif axis == 1:

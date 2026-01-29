78225aa8ef38493b5cb0ca34d218df0e8bb30353
ShamilAliyew <shamil.aliyev005@gmail.com>
2026-01-30 02:41:53 +0400

    Done

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 """a function def add_arrays(arr1, arr2):
  that adds two arrays element-wise"""
-
-
 shape = __import__('2-size_me_please').matrix_shape
+
+
 def add_arrays(arr1, arr2):
+    """a function def add_arrays(arr1, arr2):
+     that adds two arrays element-wise"""
     sum_arr = []
     if shape(arr1) != shape(arr2):
         return None

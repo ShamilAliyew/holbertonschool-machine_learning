3eb7e0abe9cf1cddb3d9295e9cabd3426382e4bd
ShamilAliyew <shamil.aliyev005@gmail.com>
2026-01-30 02:06:26 +0400

    Done

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -8,9 +8,9 @@ def matrix_shape(matrix):
     """a function def matrix_shape(matrix):
      that calculates the shape of a matrix"""
     matrix_shape = []
-    while isinstance(matrix ,list):
+    while isinstance(matrix, list):
         matrix_shape.append(len(matrix))
-        if len(matrix)==0:
+        if len(matrix) == 0:
             break
         matrix = matrix[0]
     return matrix_shape

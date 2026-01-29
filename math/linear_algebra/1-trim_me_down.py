9de81187ec4a5703dc1210826dc6ddcb38d9fd22
ShamilAliyew <shamil.aliyev005@gmail.com>
2026-01-30 00:33:00 +0400

    Done

---
+++ math/linear_algebra/1-trim_me_down.py
@@ -0,0 +1,6 @@
+matrix = [[1, 3, 9, 4, 5, 8], [2, 4, 7, 3, 4, 0], [0, 3, 4, 6, 1, 5]]
+the_middle = []
+for row in matrix:
+        if len(row) >= 4:
+            the_middle.append([row[2], row[3]])
+print(the_middle)

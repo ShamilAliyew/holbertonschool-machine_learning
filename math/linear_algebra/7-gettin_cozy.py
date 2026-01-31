67e5d35c4b19241a46378433b1ff7552e7aa9579
ShamilAliyew <shamil.aliyev005@gmail.com>
2026-01-31 04:08:00 +0400

    Done

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -9,10 +9,7 @@ def cat_matrices2D(mat1, mat2, axis=0):
      that concatenates two matrices along a specific axis"""
     if not mat1 or not mat2:
         return None
-
-    if axis == 0:
-        if len (mat1[0]) != len(mat2[0]):
-            return None
+    if len (mat1[0]) == len(mat2[0]) and axis == 0:
         result = []
         for row in mat1:
             result.append(row[:])
@@ -20,13 +17,10 @@ def cat_matrices2D(mat1, mat2, axis=0):
             result.append(row[:])
         return result
 
-    if axis == 1:
-        if (len(mat1) != len(mat2)):
-            return None
-
+    elif (len(mat1) == len(mat2)) and axis == 1:
         cat_metrice = []
         for i in range(len(mat1)):
             cat_metrice.append(mat1[i] + mat2[i])
         return cat_metrice
-    return None
-
+    else:
+        return None

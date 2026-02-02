011ebcf3b7647d6f793f8274251fe4b3208daa75
ShamilAliyew <shamil.aliyev005@gmail.com>
2026-02-02 23:22:09 +0400

    Done

--- math/advanced_linear_algebra/4-inverse.py
+++ math/advanced_linear_algebra/4-inverse.py
@@ -12,8 +12,10 @@ def inverse(matrix):
     determinant_of_matrix = determinant(matrix)
     if determinant_of_matrix == 0:
         return None
-    inverse_matrix = [[0 for _ in range(len(matrix))] for _ in range(len(matrix))]
+    inverse_matrix = [[0 for _ in range(len(matrix))]
+                      for _ in range(len(matrix))]
     for i in range(len(adjugate_matrix)):
         for j in range(len(adjugate_matrix)):
-            inverse_matrix[i][j] = adjugate_matrix[i][j] / determinant_of_matrix
+            inverse_matrix[i][j] =\
+                adjugate_matrix[i][j] / determinant_of_matrix
     return inverse_matrix

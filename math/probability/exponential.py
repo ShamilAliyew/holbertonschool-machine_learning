fd836d6bd9d6e203953cb588ed384e8b8f21e2c5
ShamilAliyew <shamil.aliyev005@gmail.com>
2026-02-08 11:57:50 +0400

    Done

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -7,7 +7,7 @@ class Exponential():
     def __init__(self, data=None, lambtha=1.):
         self.data = data
         if data is None:
-            if lambtha <= 0:
+            if lambtha < 0:
                 raise ValueError("lambtha must be a positive value")
             self.lambtha = float(lambtha)
         else:
@@ -15,4 +15,4 @@ class Exponential():
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
-            self.lambtha = sum(data)/len(data)
+            self.lambtha = sum(data) / len(data)

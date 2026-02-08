c3c9f8a34e6a847f64c236b3e2a125f0f5d49d67
ShamilAliyew <shamil.aliyev005@gmail.com>
2026-02-08 15:21:42 +0400

    Done

---
+++ math/probability/binomial.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""a class Binomial that represents a binomial distribution"""
+
+
+class Binomial():
+    """a class Binomial that represents a binomial distribution"""
+
+    def __init__(self, data=None, n=1, p=0.5):
+        pass

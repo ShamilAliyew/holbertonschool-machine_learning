9b6d06f25e81e9edd62cad050278ca88fcb1f103
ShamilAliyew <shamil.aliyev005@gmail.com>
2026-04-12 01:57:43 +0400

    Done

---
+++ pipeline/data_augmentation/0-flip.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" a function def flip_image(image): that flips an image horizontally"""
+
+import tensorflow as tf
+
+def flip_image(image):
+    """
+    Flips an image horizontally.
+
+    Args:
+        image (tf.Tensor): A 3D tf.Tensor containing the image to flip.
+
+    Returns:
+        tf.Tensor: The flipped image.
+    """
+    return tf.image.flip_left_right(image)

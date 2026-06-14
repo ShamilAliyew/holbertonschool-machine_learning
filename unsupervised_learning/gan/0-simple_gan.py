b56302b42dea567a6c3baed88c1d9bc515bddcd9
ShamilAliyew <shamil.aliyev005@gmail.com>
2026-06-14 19:18:58 +0400

    Done

--- unsupervised_learning/gan/0-simple_gan.py
+++ unsupervised_learning/gan/0-simple_gan.py
@@ -9,7 +9,7 @@ class Simple_GAN(keras.Model):
 
     def __init__(self, generator, discriminator, latent_generator, real_examples, batch_size=200, disc_iter=2,
                  learning_rate=.005):
-        super().__init__()  # run the __init__ of keras.Model first.
+        super().__init__()
         self.latent_generator = latent_generator
         self.real_examples = real_examples
         self.generator = generator
@@ -18,29 +18,27 @@ class Simple_GAN(keras.Model):
         self.disc_iter = disc_iter
 
         self.learning_rate = learning_rate
-        self.beta_1 = .5  # standard value, but can be changed if necessary
-        self.beta_2 = .9  # standard value, but can be changed if necessary
+        self.beta_1 = .5
+        self.beta_2 = .9
 
         # define the generator loss and optimizer:
         self.generator.loss = lambda x: tf.keras.losses.MeanSquaredError()(x, tf.ones(x.shape))
         self.generator.optimizer = keras.optimizers.Adam(learning_rate=self.learning_rate, beta_1=self.beta_1,
                                                          beta_2=self.beta_2)
-        self.generator.compile(optimizer=generator.optimizer, loss=generator.loss)
+        self.generator.compile(optimizer=self.generator.optimizer, loss=self.generator.loss)
 
         # define the discriminator loss and optimizer:
         self.discriminator.loss = lambda x, y: tf.keras.losses.MeanSquaredError()(x, tf.ones(
             x.shape)) + tf.keras.losses.MeanSquaredError()(y, -1 * tf.ones(y.shape))
         self.discriminator.optimizer = keras.optimizers.Adam(learning_rate=self.learning_rate, beta_1=self.beta_1,
                                                              beta_2=self.beta_2)
-        self.discriminator.compile(optimizer=discriminator.optimizer, loss=discriminator.loss)
+        self.discriminator.compile(optimizer=self.discriminator.optimizer, loss=self.discriminator.loss)
 
-    # generator of real samples of size batch_size
     def get_fake_sample(self, size=None, training=False):
         if not size:
             size = self.batch_size
         return self.generator(self.latent_generator(size), training=training)
 
-    # generator of fake samples of size batch_size
     def get_real_sample(self, size=None):
         if not size:
             size = self.batch_size
@@ -48,8 +46,28 @@ class Simple_GAN(keras.Model):
         random_indices = tf.random.shuffle(sorted_indices)[:size]
         return tf.gather(self.real_examples, random_indices)
 
-    # overloading train_step()    
     def train_step(self, useless_argument):
-
-        # 1. Diskriminatorun öyrədilməsi (disc_iter dəfə təkrarlanır)
+        # Discriminator training loop
         for _ in range(self.disc_iter):
+            with tf.GradientTape() as tape:
+                real_samples = self.get_real_sample()
+                fake_samples = self.get_fake_sample(training=True)
+
+                real_preds = self.discriminator(real_samples, training=True)
+                fake_preds = self.discriminator(fake_samples, training=True)
+
+                discr_loss = self.discriminator.loss(real_preds, fake_preds)
+
+            discr_gradients = tape.gradient(discr_loss, self.discriminator.trainable_variables)
+            self.discriminator.optimizer.apply_gradients(zip(discr_gradients, self.discriminator.trainable_variables))
+
+        # Generator training step
+        with tf.GradientTape() as tape:
+            fake_samples = self.get_fake_sample(training=True)
+            gen_preds = self.discriminator(fake_samples, training=False)
+            gen_loss = self.generator.loss(gen_preds)
+
+        gen_gradients = tape.gradient(gen_loss, self.generator.trainable_variables)
+        self.generator.optimizer.apply_gradients(zip(gen_gradients, self.generator.trainable_variables))
+
+        return {"discr_loss": discr_loss, "gen_loss": gen_loss}

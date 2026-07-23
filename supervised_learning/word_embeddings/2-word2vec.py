c2e36e9f0ca778976ef83c073d15d97391411f37
ShamilAliyew <shamil.aliyev005@gmail.com>
2026-07-23 16:04:21 +0400

    Done

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -28,15 +28,11 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
         window=window,
         negative=negative,
         sg=not cbow,
+        epochs=epochs,
         seed=seed,
         workers=workers,
         sorted_vocab=0
     )
     model.build_vocab(sentences)
-    model.train(
-        sentences,
-        total_examples=model.corpus_count,
-        epochs=epochs
-    )
 
     return model

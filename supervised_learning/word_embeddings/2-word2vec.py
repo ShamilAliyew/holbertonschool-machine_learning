8c871c61279cd23b328bbc4a9edb9812cb1b0878
ShamilAliyew <shamil.aliyev005@gmail.com>
2026-07-23 16:00:25 +0400

    Done

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -29,7 +29,8 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
         negative=negative,
         sg=not cbow,
         seed=seed,
-        workers=workers
+        workers=workers,
+        sorted_vocab=0
     )
     model.build_vocab(sentences)
     model.train(
